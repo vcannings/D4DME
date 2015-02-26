542abbd9f194eb27b867fdd9156030c23ad8cf3a
vcannings <veritycannings@gmail.com>
2015-02-26 19:05:23 +0000

    Recipe form is now working

---
+++ database-submit-tests/public/submit.php
@@ -0,0 +1,144 @@
+<!-- Including several files into the index file -->
+<?php require_once("../includes/connect.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+<?php include_once("../includes/templates/header.php"); ?>    
+
+<?php
+//initial defining of variables used on this page
+    $x = 1;
+    $titleErr = "";
+    $cooktimeErr = "";            
+    $cultureErr = "";
+    $allergyErr = "";
+    $ingredientsErr = "";
+    $instructionsErr = "";
+    $message = "* required field";
+
+    if(isset($_POST["submit"])) {
+        $title = ($_POST["recipe_title"]);
+        $cooktime = ($_POST["recipe_cooktime"]);
+        $culture = ($_POST["recipe_culture"]);
+        $allergy = ($_POST["recipe_allergy"]);
+        $ingredients = ($_POST["recipe_ingredients"]);
+        $instructions = ($_POST["recipe_instructions"]);
+    } else {
+        $title = "";
+        $cooktime = "";
+        $culture = "";
+        $allergy = "";
+        $ingredients = "";
+        $instructions = "";
+    }
+
+//validating the inputted data using a custom function and a few php functions
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $title = ucfirst($title);
+        }
+        $culture = refine_input($_POST["recipe_culture"]);
+        $culture = ucfirst($culture);   
+?>
+    
+<?php
+//testing to see whether the entry fields are empty, if so not allowing them to be submitted to database table
+    if(isset($_POST["submit"])) {
+
+        if(empty($title)) {
+            $titleErr = "Title is required";
+        } else if(empty($cooktime)) {
+            $cooktimeErr = "Cook time is required";
+        } else if(empty($culture)) {
+            $cultureErr = "Culture is required";
+        } else if(empty($allergy)) {
+            $allergyErr = "Allergy warning is required";
+        } else if(empty($ingredients)) {
+            $ingredientsErr = "Ingredients are required";
+        } else if(empty($instructions)) {
+            $instructionsErr = "Instructions are required";
+        } else if($x == 1) { 
+            $query = "INSERT INTO recipe (recipe_title, recipe_cooktime, recipe_culture, recipe_allergy, recipe_ingredients, recipe_instructions) VALUES ('{$title}', '{$cooktime}', '{$culture}', '{$allergy}', '{$ingredients}', '{$instructions}')";
+            $result = mysqli_query($connection, $query);
+
+            if($result) {
+               $message = "Thank you, your recipe has been added!";
+            } else {
+               $message = "Sorry, something went wrong";
+            }
+            $title = "";
+            $cooktime = "";
+            $culture = "";
+            $allergy = "";
+            $ingredients = "";
+            $instructions = "";
+        }
+    }
+?>
+            
+<div id="wrapper">
+    <div id="header">
+        <h3>WEBSITE</h3>
+    </div>
+    <div id="box">
+            <h1>Sign Up</h1>
+            <hr>
+            
+<!-- displaying whatever the message variable happens to be at the given time -->
+                <?php if(isset($message)) { ?>
+                <div id="smallbox">
+                <p><?php echo $message; ?></p>
+                </div>
+               <?php } ?>
+           
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+                <h2>Title:</h2><input class="formExample" placeholder="e.g Pasta Bake" type="text" value="<?php echo $title;?>" name="recipe_title" />
+                <span class="error">* <?php echo $titleErr;?></span>
+                <h2>Cook Time:</h2><select name="recipe_cooktime">
+                <option value="5 minutes">5 minutes</option>
+                <option value="10 minutes">10 minutes</option>
+                <option value="15 minutes">15 minutes</option>
+                <option value="20 minutes">20 minutes</option>
+                <option value="25 minutes">25 minutes</option>
+                <option value="30 minutes">30 minutes</option>
+                <option value="35 minutes">35 minutes</option>
+                <option value="40 minutes">40 minutes</option>
+                <option value="45 minutes">45 minutes</option>
+                <option value="50 minutes">50 minutes</option>
+                <option value="55 minutes">55 minutes</option>
+                <option value="60 minutes">60 minutes</option>
+                <option value="60 minutes +">60 minutes +</option>
+                </select>
+                <span class="error">* <?php echo $cooktimeErr; ?></span>
+                <h2>Culture:</h2><select name="recipe_culture">
+                <option value="American">American</option>
+                <option value="British">British</option>
+                <option value="Chinese">Chinese</option>
+                <option value="French">French</option>
+                <option value="German">German</option>
+                <option value="Italian">Italian</option>
+                <option value="Indian">Indian</option>
+                <option value="Japanese">Japanese</option>
+                <option value="Korean">Korean</option>
+                <option value="Spanish">Spanish</option>
+                <option value="Turkish">Turkish</option>
+                <option value="Other">Other</option>
+                </select>
+                <span class="error">* <?php echo $cultureErr; ?></span>
+                <h2>Allergy Warning:</h2><select name="recipe_allergy">
+                <option value="yes">Yes</option>
+                <option value="no">No</option>
+                </select>
+                <span class="error">* <?php echo $allergyErr; ?></span>
+                <h2>Ingredients:</h2><textarea rows="3" cols="40" class="formExample" placeholder="Ingredient one, ingredient two..." value="<?php echo $ingredients;?>" name="recipe_ingredients"></textarea>
+                <span class="error">* <?php echo $ingredientsErr; ?></span>
+                <h2>Instructions:</h2><textarea rows="8" cols="40" class="formExample" placeholder="1) instruction,  2) instruction..." value="<?php echo $instructions;?>" name="recipe_instructions"></textarea>
+                <span class="error">* <?php echo $instructionsErr; ?></span>
+                <br><br>
+                <input class="buttonExample" type="submit" value="Submit" name="submit" />
+            </form>
+    </div>
+    <br><br><br><br><br>
+    <div id="footer">
+    </div>
+ </div>
+
+
+<?php include_once("../includes/templates/footer.php"); ?>      
