105b76545f90caaf1ea0fb981f21351580061509
kylejenkinsdmd <xdkylexd@hotmail.co.uk>
2015-03-25 09:01:47 +0000

    Update functions.php

--- database-submit-tests/includes/functions.php
+++ database-submit-tests/includes/functions.php
@@ -1,5 +1,11 @@
 <!-- creating a function that validates inputted data -->
-<?php 
+<?php
+
+	function redirectTo($newLocation) {
+	  header("Location: " . $newLocation);
+	  exit;
+	}
+
     function refine_input($data) {
           $data = trim($data);
           $data = stripslashes($data);
@@ -7,4 +13,3 @@
           return $data;
     }
 ?>
-
