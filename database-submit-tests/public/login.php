983c6753edb8989db253e1e8b8ad8079e754d90d
kylejenkinsdmd <xdkylexd@hotmail.co.uk>
2015-03-25 08:59:39 +0000

    Update login.php

--- database-submit-tests/public/login.php
+++ database-submit-tests/public/login.php
@@ -3,30 +3,27 @@
     require_once("../includes/connect.php");
     require_once("../includes/functions.php");
  ?>
-    
-  <?php 
-    if(isset($_POST["submit"])){
-        $username = $_POST["username"];
-        $password = $_POST["password"];
-        
-        $query = "SELECT * FROM user WHERE usermane='{$username}' AND password='{$password}' LIMIT 1";
-        
-        $result = mysqli_query($connection, $query);
-        
-        if($result){
-            $_SESSION["message"] = "Success, Welcome back {$user["username"]}";
-            $_SESSION["username"] = $user["username"];
-            $_SESSION["user_id"] = $user["id"];
-            redirectTo("index.php");
-        } else{
-            $_SESSION["message"] = "Wrong username/password";
-        }
-        
-        echo "<br/><a href='logout.php>Logout</a>";
-       
-    } 
- ?>
 
+  <?php
+    if(isset($_POST["submit"])){/*K: if the post is told to submit then..*/
+
+        $username = $_POST["username"]; /* the username and password given in the*/
+    	$password = $_POST["password"]; /* form will become varibles to check the database*/
+
+    	$query = "SELECT * FROM user WHERE user_username='{$username}' AND user_password='{$password}' LIMIT 1";/*K: this is querying the database to see if the given data matches any on the database and to limit the amount that come back to one */
+    	$result = mysqli_query($connection, $query);
+
+		if($user = mysqli_fetch_assoc($result)) { /*K: if the given username and */
+            $_SESSION["user"] = $user["user_username"];/*password start a session*/
+            $_SESSION["user_id"] = $user["user_id"];/*that uses the username an id*/
+            $_SESSION["message"] = "Successfully logged in. Welcome back {$user["user_username"]}.";/*and show a message so the user knows that they are logged in*/
+        } else { /*however if the username and password dont match let the user know they there havent logged in*/
+            $_SESSION["message"] = "Sorry, wrong username/password.";
+        }
+		redirectTo("index.php");
+	}
+
+ ?>
 
 <!doctype html>
 <html>
@@ -34,23 +31,23 @@
         <title>Format Code</title>
         <link href="css/styles.css" rel="stylesheet" >
     </head>
-    
+
     <body>
-        <div class="container">  
-            
+        <div class="container">
+
             <div class="box">
-                <?php 
+                <?php
                     if(isset($message)) {
                         echo $message;
                     }
                 ?>
             </div>
-            
+
             <form action="login.php" method="post">
                 username: <input type="text" name="username" value="" />
                 password: <input type="text" name="password" value="" />
-                    <input type="submit" name="submit" value="Submit" />
+                <input type="submit" name="submit" value="Submit" />
             </form>
         </div>
     </body>
-</html>            
+</html>
