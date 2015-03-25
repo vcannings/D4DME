30955b6744c1d19e113dd27a7d530108d16acb26
vcannings <veritycannings@gmail.com>
2015-03-25 21:05:02 +0000

    Updating the code so that the message and redirect work properly

--- database-submit-tests/includes/templates/header.php
+++ database-submit-tests/includes/templates/header.php
@@ -2,8 +2,8 @@
 <html>
     <head>
         <title>Project</title>
+        <?php require_once("../includes/session.php"); ?>
         <link href="css/normal.css" type="text/css" rel="stylesheet">
         <link href="css/styles.css" type="text/css" rel="stylesheet">
     </head>
     <body>
-    <?php echo message(); ?>
