9594cc0c31ea4aa79996a644f5445754d8fc2cf7
vcannings <veritycannings@gmail.com>
2015-03-28 02:11:35 +0000

    Update functions.php

--- database-submit-tests/includes/functions.php
+++ database-submit-tests/includes/functions.php
@@ -12,4 +12,12 @@
           $data = htmlspecialchars($data);
           return $data;
     }
+
+    function GetSingleRecipe($recipeID) {
+        global $connection;
+        $query = "SELECT * FROM recipe WHERE recipe_id='{$recipeID}'";
+        $result = mysqli_query($connection, $query);
+        return $result;
+    }
+
 ?>
