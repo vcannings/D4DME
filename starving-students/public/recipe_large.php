d2cae988b36012c475da98c7c92f6ed76c23042f
vcannings <veritycannings@gmail.com>
2015-03-30 20:57:36 +0100

    all of the php now works with the css

--- starving-students/public/recipe_large.php
+++ starving-students/public/recipe_large.php
@@ -1,6 +1,47 @@
+<!-- V: Including several files into the index file using a php function -->
+<?php require_once("../includes/connect.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+<?php include_once("../includes/templates/header.php"); ?> 
+        
+            <div class="side-nav">
+                <div class="side-nav-header">
+                    <h2>FIND A RECIPE</h2>
+                </div><br>
+                <form method="get" name="cooktime" action="index.php">
+      <label><strong>COOK TIME</strong></label><br>
+      <input type="text" name="cooktime" placeholder="e.g 30 minutes" size="10" maxlength="120">
+      <input type="submit" value="Search">
+    </form>
+    <br>
+  <form method="get" name="cuisine" action="index.php">
+      <label><strong>CUISINE</strong></label><br>
+      <input type="text" name="cuisine" placeholder="e.g Italian" size="10" maxlength="120">
+      <input type="submit" value="Search">
+    </form>
+    <br>
+<form method="get" name="ingredient" action="index.php">
+      <label><strong>INGREDIENTS</strong></label><br>
+      <input type="text" name="ingredient" placeholder="e.g Chicken" size="10" maxlength="120">
+      <input type="submit" value="Search">
+    </form>
+    <br>
+<form method="get" name="allergy" action="index.php">
+      <label><strong>ALLERGY WARNING?</strong></label><br>
+      <input type="text" name="allergy" placeholder="Yes/No" size="10" maxlength="120">
+      <input type="submit" value="Search">
+    </form>
+    <br><br>
+    <?php if(isset($_SESSION["user"])) { ?>
+<a href="submit.php">CREATE YOUR OWN</a>
+    <?php } else { ?>
+    <?php } ?>               
+      <br><br><br>          
+    </div>
+
 <!-- V: This div is included in the index.php page to display each recipe seperately -->
-<div class="recipe-box-large">
-    <h1><a href="selected_recipe.php?recipe_id=<?php echo ($row["recipe_id"]); ?>"><?php echo ($row["recipe_title"]); ?></a></h1>
+<div class="content-container">
+<div class="contentLarge">
+    <h1><?php echo ($row["recipe_title"]); ?></a></h1>
         <hr>
 <!-- V: our $row variable was introduced in index.php and is an array of all our table data -->
 	<?php
@@ -12,11 +53,21 @@
   	?>
     
 <!-- V: The php block above is saying that if the recipe_image column is not empty then to use the URL in it, but if it is empty then use a default image -->
-    <img src="<?php echo $imageName; ?>" /><br>
+    <img src="<?php echo $imageName; ?>" />
 <!-- Asign either result as a variable and echo in imsge tags -->
     <h2>Cook time</h2><br><p><?php echo ($row["recipe_cooktime"]); ?></p><br><br>
     <h2>Cuisine</h2><br><p><?php echo ($row["recipe_culture"]); ?></p><br><br>
-    <h2>Allergy warning</h2><br><p><?php echo ($row["recipe_allergy"]); ?></p><br><br><br><br>
+    <h2>Allergy warning</h2><br><p><?php echo ($row["recipe_allergy"]); ?></p><br><br>
     <h2>Description:</h2><br>
     <p><?php echo ($row["recipe_description"]); ?></p>
+    <br><br>
+    <h2>Ingredients:</h2><br>
+    <p><?php echo ($row["recipe_ingredients"]); ?></p>
+    <br><br><br><br>
+    <h2>Instructions:</h2><br>
+    <p><?php echo ($row["recipe_instructions"]); ?></p>
 </div>
+<br><br><br><br><br><br>
+</div>
+<br><br><br><br><br><br>
+<?php include_once("../includes/templates/footer.php"); ?>
--- starving-students/public/selected_recipe.php
+++ starving-students/public/selected_recipe.php
@@ -12,20 +12,10 @@
 ?>
 
 
-<div id="wrapper">
-    <div id="header">
-        <h3>WEBSITE</h3>
-        <?php if(isset($_SESSION["user"])) { ?>
-        	<a href="logout.php" class="buttonExample" style="float: right;">Logout</a>
-        <?php } else { ?>
-		<a href="signup.php" class="buttonExample" style="float: right;">Sign Up</a>
-	        <a href="login.php" class="buttonExample" style="float: right"> Log In</a>
-	<?php } ?>
-		
-    </div><br><br><br><br>
+    <br>
     <p><?php echo message(); ?></p>    
     
-    <div id="container">
+    
       
         <?php 
             $result = GetSingleRecipe($recipeID);
@@ -35,43 +25,10 @@
             }
         ?>
    
-    </div>
-    
-<div id="search-bar">
-    <h2>Find a recipe</h2>
-    <hr>
-    <form method="get" name="cooktime" action="index.php">
-      <label>Cook Time</label><br>
-      <input type="text" name="cooktime" placeholder="e.g 30 minutes" size="12" maxlength="120">
-      <input type="submit" value="Search">
-    </form>
-    <br>
-  <form method="get" name="cuisine" action="index.php">
-      <label>Cuisine</label><br>
-      <input type="text" name="cuisine" placeholder="e.g Italian" size="12" maxlength="120">
-      <input type="submit" value="Search">
-    </form>
-    <br>
-<form method="get" name="ingredient" action="index.php">
-      <label>Ingredients</label><br>
-      <input type="text" name="ingredient" placeholder="e.g Chicken" size="12" maxlength="120">
-      <input type="submit" value="Search">
-    </form>
-    <br>
-<form method="get" name="allergy" action="index.php">
-      <label>Allergy Warning?</label><br>
-      <input type="text" name="allergy" placeholder="Yes/No" size="12" maxlength="120">
-      <input type="submit" value="Search">
-    </form>
-    <br>
-    <?php if(isset($_SESSION["user"])) { ?>
-<a href="submit.php" class="buttonExample">Submit your own!</a>
-    <?php } else { ?>
-    <?php } ?>
-</div>
+
 
     <div id="footer">
     </div>
-</div>
+
 
 <?php include_once("../includes/templates/footer.php"); ?> 
