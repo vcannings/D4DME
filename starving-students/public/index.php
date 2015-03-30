4b52f90dc57e63885f9751d2c5a959cab5debdb9
vcannings <veritycannings@gmail.com>
2015-03-30 01:43:47 +0100

    Update and rename index.html to index.php

---
+++ starving-students/public/index.php
@@ -0,0 +1,138 @@
+<?php include_once("../includes/templates/header.php"); ?> 
+        <div class="container">
+            <div class="header">
+                <div class="logo">
+                      <img src="images/Logo.png">
+                </div>
+                <div class="title">
+                    <h1>STARVING STUDENTS</h1>
+                    <img src="images/chalk-boarder.png">
+                </div>
+                <div class="menu-glyph">
+                    <img src="images/chalk-boarder.png"/>
+                </div>
+            </div>
+            <div class="nav-bar-left">
+                <ul>
+                    <li><button type="button">Home</button></li>
+                    <li><button type="button">About</button></li>
+                    <li><button type="button">Menu</button></li>
+                </ul>
+            </div>
+            <div class="nav-bar-right">
+                <ul>
+                    <li><button type="button">Log in</button></li>
+                    <li><button type="button">Sign Up</button></li>
+                </ul>
+            </div>
+            <div class="side-nav">
+                <div class="side-nav-header">
+                    <h2>SELECT YOUR OPTIONS</h2>
+                </div>
+                <ul>
+                    <li>
+                        <form method="get" name="cooktime" action="index.php">
+          <label>Cook Time</label><br>
+          <input type="text" name="cooktime" placeholder="e.g 30 minutes" size="12" maxlength="120">
+          <input type="submit" value="Search">
+        </form>
+                        </li>
+                        <li><form method="get" name="cuisine" action="index.php">
+          <label>Cuisine</label><br>
+          <input type="text" name="cuisine" placeholder="e.g Italian" size="12" maxlength="120">
+          <input type="submit" value="Search">
+        </form>
+    </li>
+                    <li><form method="get" name="ingredient" action="index.php">
+          <label>Ingredients</label><br>
+          <input type="text" name="ingredient" placeholder="e.g Chicken" size="12" maxlength="120">
+          <input type="submit" value="Search">
+        </form>
+                        </li>
+                        <li><form method="get" name="allergy" action="index.php">
+          <label>Allergy Warning?</label><br>
+          <input type="text" name="allergy" placeholder="Yes/No" size="12" maxlength="120">
+          <input type="submit" value="Search">
+        </form>
+                        </li>
+                    <li>
+                        <button type="button2">CREATE YOUR OWN</button>
+                    </li>
+                </ul>
+            </div>
+            <div class="content-container">
+                <div class="content">
+                    <a href="#">
+                        <h1>Dishes</h1>
+                        <img src="/Users/compaq/Downloads/placeholder-2.png"/>
+                        <p>Lorem ipsum dolor sit amet, cu meis vocent vix, dolor blandit placerat vis et.</p>
+                     </a>
+                </div>   
+                <div class="content">
+                    <a href="#">
+                        <h1>Dishes</h1>
+                        <img src="/Users/compaq/Downloads/placeholder-2.png"/>
+                        <p>Lorem ipsum dolor sit amet, cu meis vocent vix, dolor blandit placerat vis et.</p>
+                     </a>
+                </div>   
+                <div class="content">
+                    <a href="#">
+                        <h1>Dishes</h1>
+                        <img src="/Users/compaq/Downloads/placeholder-2.png"/>
+                        <p>Lorem ipsum dolor sit amet, cu meis vocent vix, dolor blandit placerat vis et.</p>
+                     </a>
+                </div>   
+                <div class="content">
+                    <a href="#">
+                        <h1>Dishes</h1>
+                        <img src="/Users/compaq/Downloads/placeholder-2.png"/>
+                        <p>Lorem ipsum dolor sit amet, cu meis vocent vix, dolor blandit placerat vis et.</p>
+                     </a>
+                </div>   
+                <div class="content">
+                    <a href="#">
+                        <h1>Dishes</h1>
+                        <img src="/Users/compaq/Downloads/placeholder-2.png"/>
+                        <p>Lorem ipsum dolor sit amet, cu meis vocent vix, dolor blandit placerat vis et.</p>
+                     </a>
+                </div>   
+                <div class="content">
+                    <a href="#">
+                        <h1>Dishes</h1>
+                        <img src="/Users/compaq/Downloads/placeholder-2.png"/>
+                        <p>Lorem ipsum dolor sit amet, cu meis vocent vix, dolor blandit placerat vis et.</p>
+                     </a>
+                </div>   
+                <div class="content">
+                    <a href="#">
+                        <h1>Dishes</h1>
+                        <img src="/Users/compaq/Downloads/placeholder-2.png"/>
+                        <p>Lorem ipsum dolor sit amet, cu meis vocent vix, dolor blandit placerat vis et.</p>
+                     </a>
+                </div>   
+                <div class="content">
+                    <a href="#">
+                        <h1>Dishes</h1>
+                        <img src="/Users/compaq/Downloads/placeholder-2.png"/>
+                        <p>Lorem ipsum dolor sit amet, cu meis vocent vix, dolor blandit placerat vis et.</p>
+                     </a>
+                </div>   
+                <div class="content">
+                    <a href="#">
+                        <h1>Dishes</h1>
+                        <img src="/Users/compaq/Downloads/placeholder-2.png"/>
+                        <p>Lorem ipsum dolor sit amet, cu meis vocent vix, dolor blandit placerat vis et.</p>
+                     </a>
+                </div>   
+                <div class="content">
+                    <a href="#">
+                        <h1>Dishes</h1>
+                        <img src="/Users/compaq/Downloads/placeholder-2.png"/>
+                        <p>Lorem ipsum dolor sit amet, cu meis vocent vix, dolor blandit placerat vis et.</p>
+                     </a>
+                </div>   
+                <br><br>
+            </div>
+        </div>
+        <div class="footer"></div>
+<?php include_once("../includes/templates/footer.php"); ?> 
