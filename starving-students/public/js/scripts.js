4d73835373b67aae70751fa541d03227b5dce257
kylejenkinsdmd <xdkylexd@hotmail.co.uk>
2015-04-18 17:02:22 +0100

    Update scripts.js

--- starving-students/public/js/scripts.js
+++ starving-students/public/js/scripts.js
@@ -1,9 +1,27 @@
-$(document).ready(function() {
-    $('.nav-bar-left').hover(
-        function(){
-            $(this).addClass('nav-bar-hover');
-        });
-        function(){
-            $(this).removeClass('nav-bar-hover');
-        });
+$(function() {
+    
+    $('ul #navbar').mouseover(function(){
+        $(this).css({"color": "grey"});
+    });
+    $('ul #navbar').mouseleave(function(){
+        $(this).css({"color": "white"});
+    });
+    
+    
+    $('.content').mouseenter(function(){
+        $(this).css({"background": "#bfbfbf"});
+    });
+    $('.content img').mouseenter(function(){
+        $(this).css({"height":"155px", "width":"155px"});
+    });
+    
+    
+    $('.content').mouseleave(function(){
+        $(this).css({"background":"white"});
+    });
+    $('.content img').mouseleave(function(){
+        $(this).css({"height":"150px", "width":"150px"});
+    });
+    
+    
 });
